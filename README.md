352661cc94904d812cf26c31de93a615115c054a
Ryan McMillan <ryan.j.mcmillan34@gmail.com>
2020-10-22 22:00:59 +1000

    Implemented Message on bot online

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # DiscordBot
-A Discord chat bot written in python
+by Ryan McMillan 2020.  
+A Discord chat bot written in python utilizing discord.py and discord APIs.  
+  
+## Table of Contents
+- [DiscordBot](#discordbot)
+  - [Table of Contents](#table-of-contents)
+  - [Introduction](#introduction)
+  - [Features](#features)
+  - [Features to Add](#features-to-add)
+  - [Commands](#commands)
+  
+## Introduction
+This project was created with the intention of honing my python programming, and artificial intelligence/machine learning skills.  
+I intend this bot to be a testing ground for textual NLP models and voice recognition.  
+The idea to create a chat bot came from aceking007's [#100ProjectssOfCode](https://github.com/aceking007/100ProjectsOfCode) repository.  
+  
+## Features
+Currently RizzBot has the following engrained functionalities:  
+[x] Welcome a new user to the discord server.  
+  
+## Features to Add
+The following is a list of features that I intend to add to this project.  
+[] Link to this README.md on github in the !help command  
+[] Scissors Paper Rock vs RizzBot  
+[] Scissors Paper Rock vs another server member  
+[] Adjudicate games of odds between server members  
+[] Ask Jeopardy Questions, wait and tell the answer  
+[] [Play games of Trivia](https://opentdb.com/browse.php)
+[] Endow with sentience  
+  
+## Commands
+All commands for Rizzbot are prefixed with an '!'. The following commands can be sent through any text channel or Direct Message to RizzBot: 
+|Command      |                                                     Description                       |
+|---          |---                                                                                    |
+|!roll_dice   | A function requesting RizzBot return a number between 1 and 6.                        |
+|!SPR         | Allows the user to play Scissors Paper Rock with Rizzbot  (UNDER CONSTRUCTION)        |
+|!help        | Shows the user a list of available commands                                           |
--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import os
 import discord
 import random
+import time
 from discord.ext.commands import Bot
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -11,12 +12,13 @@ load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+GENERAL = int(os.getenv('GENERAL_CHANNEL'))
 
 intents = discord.Intents.default()
 intents.members = True
 
 description = ''' A discord chat bot written in python by Rizz for the Circle Jerk Server
-
+README can be found here: https://github.com/rmcmillan34/DiscordBot/blob/main/README.md
 '''
 
 bot = commands.Bot(command_prefix='!', description=description, intents=intents)
@@ -38,11 +40,6 @@ async def on_ready():
 	members = '\n - '.join([member.name for member in guild.members])
 	print(f'Guild Members:\n - {members}')
 
-	# Send a broadcast notifying of bot online status
-#	for channel in bot.get_all_channels():
-#		if channel.permissions_for(bot):
-#			await bot.Messageable(channel, "Yoooooooo, Im back online")
-
 
 @bot.event
 async def on_member_join(member):
@@ -51,19 +48,33 @@ async def on_member_join(member):
         f'Wassup {member.name}, ya fuck. Welcome to the Circle Jerk discord server.'
     )
 
+
 @bot.command(name='hello', category='Useless')
 async def hello(ctx):
+	'''Return a personalised greeting.'''
 	await ctx.send('Hello {0.author.mention}'.format(ctx))
+
 
 @bot.command(name='roll_dice')
 async def roll_dice(ctx):
+	'''Return integer between 1 and 6.'''
+	await ctx.send("Rolling Dice..")
 	response = random.randint(1, 6)
-	await ctx.send(str(response) + ':scissors:')
+	await ctx.send(str(response))
+
 
 @bot.command(name='SPR')
 async def scissors_paper_rock(ctx):
 	'''Return a random scissors, paper or rock emoji'''
+	await ctx.send('Alright {0.author.mention}, lets go..'.format(ctx))
+	time.sleep(1)
+	await ctx.send("Scissors..")
+	time.sleep(1)
+	await ctx.send("Paper..")
+	time.sleep(1)
+	await ctx.send("Rock!")
 	choices = [':scissors:', ':roll_of_paper:', ':rock:']
 	await ctx.send(random.choice(choices))
+
 
 bot.run(TOKEN)
